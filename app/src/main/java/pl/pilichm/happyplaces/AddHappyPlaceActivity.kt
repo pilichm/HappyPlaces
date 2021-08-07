e041414e9f115bd6b50a99b02c96d66e35eacd4b
pilichm <michal.pilichowski@onet.pl>
2021-08-07 16:19:56 +0200

    Added: - set up add place activity.

--- app/src/main/java/pl/pilichm/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/pl/pilichm/happyplaces/AddHappyPlaceActivity.kt
@@ -2,10 +2,19 @@ package pl.pilichm.happyplaces
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_happy_place.*
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+
+        setSupportActionBar(toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "ADD HAPPY PLACE"
+        toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
     }
 }
--- app/src/main/java/pl/pilichm/happyplaces/MainActivity.kt
+++ app/src/main/java/pl/pilichm/happyplaces/MainActivity.kt
@@ -1,11 +1,20 @@
 package pl.pilichm.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabAddHappyPlace.setOnClickListener {
+            startActivity(
+                Intent(applicationContext, AddHappyPlaceActivity::class.java)
+            )
+        }
+
     }
 }
